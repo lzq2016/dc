e67eeb901c47a36bcf0fea6bb1ff78bba5df0d0a
lzq <357881371@qq.com>
2018-01-05 19:44:40 +0800

    lzq

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,10 +18,10 @@ Page({
         ],
         posts_key: [],
         layerHeight: 0,
-        foodDetailShow:'none',
-        foodDetailContent:{},
-        currentStandard:0,
-        currentSize:0
+        foodDetailShow: 'none',
+        foodDetailContent: {},
+        currentStandard: 0,
+        currentSize: 0
     },
 
     onLoad: function () {
@@ -34,31 +34,25 @@ Page({
     menuTap: function (event) {
         var that = this;
         var dishid = event.currentTarget.dataset.dishid;
-        postsData.postList.forEach(function(item){
-            if(item.dishid == dishid)
-            {
+        postsData.postList.forEach(function (item) {
+            if (item.dishid == dishid) {
                 that.setData({
                     foodDetailShow: 'block',
                     foodDetailContent: item
                 })
             }
         });
-        
-        // wx.navigateTo({
-        //     url: '../Buy/Buy?id=' + postId
-        // })
     },
     layer: function (e) {
         var that = this;
-        if (that.data.foodDetailShow == "block")
-        {
+        if (that.data.foodDetailShow == "block") {
             that.setData({
                 foodDetailShow: 'none'
             })
         }
     },
     layer1: function (e) {
-    
+
     },
     standardTap: function (e) {
         var that = this;
@@ -73,5 +67,59 @@ Page({
         that.setData({
             currentSize: code
         })
+    },
+    addtocar: function (e) {
+        var that = this;
+        var dish = {};
+        var dishlist = [];
+        var dishid = e.currentTarget.dataset.dishid;
+        postsData.postList.forEach(function (item) {
+            if (item.dishid == dishid) {
+                dish = item;
+            }
+        });
+
+        try {
+            var value = wx.getStorageSync('dishlist');
+            var hasOrdered = false;
+            if (value) {
+                // dishlist = value;
+                value.forEach(function (item) {
+                    if (item.dishid == dishid) {
+                        item.amount += 1;
+                        hasOrdered = true;
+                    }
+                });
+                !hasOrdered ? value.push(dish) : "";
+                try {
+                    wx.setStorageSync('dishlist', value);
+                    wx.showToast({
+                        title: '已加到菜篮中~',
+                        icon: 'success',
+                        image: '/images/eat.png',
+                        duration: 2000
+                    });
+                } catch (e) {
+                    console.log("存菜单失败");                    
+                }
+            }
+            else{
+                console.log("未获取到已点菜单");
+                dishlist.push(dish);
+                try {
+                    wx.setStorageSync('dishlist', dishlist);
+                    wx.showToast({
+                        title: '已加到菜篮中~',
+                        icon: 'success',
+                        image: '/images/eat.png',
+                        duration: 1000
+                    });
+                } catch (e) {
+                    console.log("存菜单失败");
+                }
+            }
+        } catch (e) {
+            
+        } 
     }
 })
